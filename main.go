d9fce877172093395eed94ca7d8439c6ab83a4bc
Aleksandr Sazonov <aleksandr.sazonov@south.rt.ru>
2019-08-09 11:33:46 +0300

    trying implement goroutines

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
   "bufio"
   "strings"
   "strconv"
+  "sync"
 )
 
 var host = "https://hackerone.com/reports/"
@@ -37,11 +38,60 @@ func loopURLs() {
         break
       }
     }
-    //fmt.Println(i)
+    fmt.Println(param)
     param++
   }
 }
 
+var id int = 104543
+
 func main() {
-    loopURLs()
+  // loopURLs()
+  var wg sync.WaitGroup
+  for j := 0; j < 10; j++ {
+    wg.Add(1)
+    go func() {
+      for i := 0; i < 1100; i++ {
+        resp, err := http.Get(host + strconv.Itoa(id))
+        if err != nil {
+          fmt.Println(err)
+        }
+        defer resp.Body.Close()
+
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+          fmt.Println(err)
+        }
+        res := string(body)
+        scan := bufio.NewScanner(strings.NewReader(res))
+        for scan.Scan() {
+          line := scan.Text()
+          if strings.Index(line, "<meta name=\"twitter") >= 0 {
+            // fmt.Println(line)
+            fmt.Println(host + strconv.Itoa(id))
+            break
+          }
+        }
+        id++
+        // fmt.Println(id)
+      }
+      wg.Done()
+    }()
+  }
+  wg.Wait()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
