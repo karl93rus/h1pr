6184eae25558ddc735bfc6150dea18e5f12e9cb5
Aleksandr Sazonov <aleksandr.sazonov@south.rt.ru>
2019-08-12 15:01:57 +0300

    conc

--- main.go
+++ main.go
@@ -1,108 +1,163 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "io/ioutil"
-  "bufio"
-  "strings"
-  "strconv"
-  "sync"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 var host = "https://hackerone.com/reports/"
 
 func loopURLs() {
-  var param int
-  param = 104543
-  for i := 0; i < 1100; i++ {
-    resp, err := http.Get(host + strconv.Itoa(param))
-    if err != nil {
-      fmt.Println(err)
-    }
+	var param int
+	param = 104543
+	for i := 0; i < 1100; i++ {
+		resp, err := http.Get(host + strconv.Itoa(param))
+		if err != nil {
+			fmt.Println(err)
+		}
 
-    defer resp.Body.Close()
+		defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-      fmt.Println(err)
-    }
-    res := string(body)
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		res := string(body)
 
-    scan := bufio.NewScanner(strings.NewReader(res))
-    for scan.Scan() {
-      line := scan.Text()
-      if strings.Index(line, "<meta name=\"twitter") >= 0 {
-        // fmt.Println(line)
-        fmt.Println(host + strconv.Itoa(param))
-        break
-      }
-    }
-    fmt.Println(param)
-    param++
-  }
+		scan := bufio.NewScanner(strings.NewReader(res))
+		for scan.Scan() {
+			line := scan.Text()
+			if strings.Index(line, "<meta name=\"twitter") >= 0 {
+				// fmt.Println(line)
+				fmt.Println(host + strconv.Itoa(param))
+				break
+			}
+		}
+		fmt.Println(param)
+		param++
+	}
 }
 
-var id int = 104500
+//var id int = 104500
+
+type id struct {
+	id int
+	m  sync.Mutex
+}
+func (i *id) Inc() {
+	i.m.Lock()
+	i.id++
+	i.m.Unlock()
+}
+func (i *id) Write(ch chan string) {
+	i.m.Lock()
+	ch <- strconv.Itoa(i.id)
+	i.m.Unlock()
+}
+
+//func checkUrl(host string, id int, ch chan string) {
+//	for i := 0; i < 500; i++ {
+//		resp, err := http.Get(host + strconv.Itoa(id))
+//		if err != nil {
+//			fmt.Println(err)
+//		}
+//		defer resp.Body.Close()
+//
+//		body, err := ioutil.ReadAll(resp.Body)
+//		if err != nil {
+//			fmt.Println(err)
+//		}
+//		res := string(body)
+//		scan := bufio.NewScanner(strings.NewReader(res))
+//		for scan.Scan() {
+//			line := scan.Text()
+//			if strings.Index(line, "<meta name=\"twitter") >= 0 {
+//				// fmt.Println(line)
+//				// fmt.Println(host + strconv.Itoa(id))
+//				ch <- host + strconv.Itoa(id)
+//				break
+//			}
+//		}
+//		id++
+//		fmt.Println(id)
+//	}
+//}
+
+
+func checkUrl(host string, id id, ch chan string) {
+	for i := 0; i < 500; i++ {
+		resp, err := http.Get(host + strconv.Itoa(id.id))
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		res := string(body)
+		scan := bufio.NewScanner(strings.NewReader(res))
+		for scan.Scan() {
+			line := scan.Text()
+			if strings.Index(line, "<meta name=\"twitter") >= 0 {
+				// fmt.Println(line)
+				// fmt.Println(host + strconv.Itoa(id))
+        ch <- host + strconv.Itoa(id.id)
+        id.Write(ch)
+				break
+			}
+		}
+		id.id++
+		fmt.Println(id.id)
+	}
+}
 
 func main() {
-  // loopURLs()
-  ch := make(chan string, 100)
-  var wg sync.WaitGroup
-  for j := 0; j < 5; j++ {
-    wg.Add(1)
-    go func() {
-      for i := 0; i < 500; i++ {
-        resp, err := http.Get(host + strconv.Itoa(id))
-        if err != nil {
-          fmt.Println(err)
-        }
-        defer resp.Body.Close()
+	// loopURLs()
+	ch := make(chan string)
+  count := id{id: 104540}
 
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-          fmt.Println(err)
-        }
-        res := string(body)
-        scan := bufio.NewScanner(strings.NewReader(res))
-        for scan.Scan() {
-          line := scan.Text()
-          if strings.Index(line, "<meta name=\"twitter") >= 0 {
-            // fmt.Println(line)
-            // fmt.Println(host + strconv.Itoa(id))
-            ch <- host + strconv.Itoa(id)
-            break
-          }
-        }
-        id++
-        fmt.Println(id)
-      }
-      wg.Done()
-    }()
+	for i := 0; i < 2; i++ {
+		go checkUrl(host, count, ch)
+	}
 
-    wg.Add(1)
-    go func() {
-      // fmt.Println(<- ch)
-      for i := range ch {
-        fmt.Println(i)
-      }
-      wg.Done()
-    }()
-  }
-  wg.Wait()
+	// for j := 0; j < 5; j++ {
+	//   go func() {
+	//     for i := 0; i < 500; i++ {
+	//       resp, err := http.Get(host + strconv.Itoa(id))
+	//       if err != nil {
+	//         fmt.Println(err)
+	//       }
+	//       defer resp.Body.Close()
+
+	//       body, err := ioutil.ReadAll(resp.Body)
+	//       if err != nil {
+	//         fmt.Println(err)
+	//       }
+	//       res := string(body)
+	//       scan := bufio.NewScanner(strings.NewReader(res))
+	//       for scan.Scan() {
+	//         line := scan.Text()
+	//         if strings.Index(line, "<meta name=\"twitter") >= 0 {
+	//           // fmt.Println(line)
+	//           // fmt.Println(host + strconv.Itoa(id))
+	//           ch <- host + strconv.Itoa(id)
+	//           break
+	//         }
+	//       }
+	//       id++
+	//       fmt.Println(id)
+	//     }
+	//   }()
+	// }
+
+	for i := range ch {
+		fmt.Println(i)
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
